cd9dee4eb4d5013fa69e903a99f22f922d616504
Sergey Klevitskiy <sergey.klevitskiy@swtecnn.com>
2020-04-27 10:53:02 +0300

    main functionality

--- lib/carbon_intensity_collector/application.ex
+++ lib/carbon_intensity_collector/application.ex
@@ -1,3 +1,4 @@
+
 defmodule CarbonIntensityCollector.Application do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
@@ -5,10 +6,14 @@ defmodule CarbonIntensityCollector.Application do
 
   use Application
 
+  alias CarbonIntensityCollector.{Repo, Scheduler, Gatherer}
+
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: CarbonIntensityCollector.Worker.start_link(arg)
-      # {CarbonIntensityCollector.Worker, arg}
+      Repo,
+      Scheduler,
+      #perform data acquisition at start up
+      {Task, fn -> Gatherer.perform_data_acquisition() end}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
---
+++ lib/carbon_intensity_collector/co2_emission_schema.ex
@@ -0,0 +1,20 @@
+defmodule CarbonIntensityCollector.Co2EmissionSchema do
+  @moduledoc """
+  Defines schema for co2_emission table and changeset for adding emission value
+  """
+
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "co2_emission" do
+    field :intensity, :integer
+    field :datetime, :string
+  end
+
+  @spec changeset(intention :: Ecto.Schema.t, params :: Keyword.t) :: Ecto.Changeset.t
+  def changeset(intention, params) do
+    intention
+    |> cast(params, __MODULE__.__schema__(:fields))
+    |> validate_required([:intensity, :datetime])
+  end
+end
